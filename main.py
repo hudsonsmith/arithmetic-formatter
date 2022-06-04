2e4e2c55092380636bb5c9ff35f3cf3ea0bfcca8
Hudson Smith <76886104+hudsonsmith@users.noreply.github.com>
2022-06-04 16:10:07 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from arithmetic_arranger import arithmetic_arranger
+
+print("""
+             (Arithmetic Formatter 1.0!)
+/\\____/\\    /
+|+    -|   /
+| _/\_ |  /
+|_\__/_|
+|--DOG-|
+
+""")
+
+while True:
+    question = input("EXPRESSION: ")
+
+    if not " + " in question or not " - " in question:
+        question = question.replace("+", " + ").replace("-", " - ")
+        
+    print(arithmetic_arranger([question], True))
